0936ddff81d3e72c88ba56120106346442df7360
byverdu <byverdu@gmail.com>
2014-08-25 01:58:44 +0100

    costumer is only hungry on midday and dinner time

--- spec/costumer_spec.rb
+++ spec/costumer_spec.rb
@@ -8,7 +8,9 @@ class Costumer
 		@hungry = false
 	end 
 
-
+	def time_to_eat! hour
+		@hungry = true if Time.now.hour === 12 || Time.now.hour === 18
+	end
 
 end
 
@@ -20,6 +22,24 @@ describe Costumer do
 
 		it "is initialized without being hungry" do
 			expect(jhon.hungry).to be false
+		end
+
+		it "knows when is lunch time" do
+			midday = Time.local(2014,8,25,12,0)
+			Timecop.freeze(midday)
+
+			jhon.time_to_eat! midday
+
+			expect(jhon.hungry).to be true
+		end
+
+		it "knows when is dinner time" do
+			dinner = Time.local(2014,8,25,18)
+			Timecop.freeze(dinner)
+
+			jhon.time_to_eat! dinner
+
+			expect(jhon.hungry).to be true
 		end
 
 	end
